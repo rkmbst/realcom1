1e9d069b5cce44e21d300f73876ce4a8754540df
rkmbst <khaouni0rabah13@gmail.com>
2026-08-15 15:58:48 +0100

    Add AppSpacing class for consistent spacing values

---
+++ lib/core/theme/app_spacing.dart
@@ -0,0 +1,15 @@
+class AppSpacing {
+  AppSpacing._();
+
+  static const double x4 = 4;
+  static const double x8 = 8;
+  static const double x12 = 12;
+  static const double x16 = 16;
+  static const double x24 = 24;
+  static const double x32 = 32;
+  static const double x48 = 48;
+
+  static const double dense = 16;
+  static const double airyMin = 20;
+  static const double airyMax = 24;
+}
