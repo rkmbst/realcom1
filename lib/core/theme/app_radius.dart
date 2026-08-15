4184fec31f6c64a3825d589c6993787f9d2678c1
rkmbst <khaouni0rabah13@gmail.com>
2026-08-15 15:59:13 +0100

    Add AppRadius class for consistent radius values

---
+++ lib/core/theme/app_radius.dart
@@ -0,0 +1,13 @@
+class AppRadius {
+  AppRadius._();
+
+  static const double button = 18;
+  static const double card = 22;
+  static const double cardLarge = 24;
+  static const double shop = 18;
+
+  static const double pill = 999;
+
+  static const double small = 12;
+  static const double medium = 16;
+}
