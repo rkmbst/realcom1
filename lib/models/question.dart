bc1c79d9dfedee4b5c438063c56e912eb135945f
rkmbst <khaouni0rabah13@gmail.com>
2026-08-17 21:46:29 +0100

    Add authorId and correctOptionId to Question model

--- lib/models/question.dart
+++ lib/models/question.dart
@@ -7,11 +7,22 @@ class Question {
   final List<QuestionOption> options;
   final String? authorName;
 
+  /// User id of the author.
+  final String? authorId;
+
+  /// Id of the correct option.
+  ///
+  /// Optional for backwards compatibility with
+  /// older poll-style questions.
+  final String? correctOptionId;
+
   const Question({
     required this.id,
     required this.text,
     required this.categoryId,
     required this.options,
     this.authorName,
+    this.authorId,
+    this.correctOptionId,
   });
 }
