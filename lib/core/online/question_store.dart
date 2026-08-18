c20addf3a09d7473bf0c6ccc30945415e96c95d8
rkmbst <khaouni0rabah13@gmail.com>
2026-08-19 00:02:10 +0100

    Fix formatting and add newline at end of file

--- lib/core/online/question_store.dart
+++ lib/core/online/question_store.dart
@@ -54,6 +54,8 @@ class QuestionStore {
           type:
               NotificationType
                   .newQuestion,
+          recipientUserId:
+              followerId,
           actorUserId:
               authorId,
           actorName:
