0742cc97a38b269ec47f11c6e60d7fb4141ae5ed
rkmbst <khaouni0rabah13@gmail.com>
2026-08-18 18:32:49 +0100

    Change app title and implement AuthGate for routing
    Updated the app title and modified the home screen to use AuthGate for authentication handling.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'core/auth/auth_session.dart';
 import 'core/theme/app_theme.dart';
+import 'screens/auth/account_switch_screen.dart';
 import 'screens/home_screen.dart';
 
 void main() {
@@ -13,10 +15,28 @@ class WheelApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Question Wheel',
+      title: 'WeLibre',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.dark(),
-      home: const HomeScreen(),
+      home: const AuthGate(),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: AuthSession.instance,
+      builder: (context, _) {
+        if (AuthSession.instance.isAuthenticated) {
+          return const HomeScreen();
+        }
+
+        return const AccountSwitchScreen();
+      },
     );
   }
 }
