860db1d445d091371af54494a91ed4d345f0464a
rkmbst <khaouni0rabah13@gmail.com>
2026-08-19 00:06:19 +0100

    Refactor main and WheelApp for theme management
    Refactor main function to initialize theme controller and change WheelApp to StatefulWidget for theme management.

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,69 @@ import 'package:flutter/material.dart';
 
 import 'core/auth/auth_session.dart';
 import 'core/theme/app_theme.dart';
+import 'core/theme/theme_controller.dart';
 import 'screens/auth/account_switch_screen.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await ThemeController.instance.load();
+
   runApp(const WheelApp());
 }
 
-class WheelApp extends StatelessWidget {
+class WheelApp extends StatefulWidget {
   const WheelApp({super.key});
+
+  @override
+  State<WheelApp> createState() =>
+      _WheelAppState();
+}
+
+class _WheelAppState
+    extends State<WheelApp> {
+  final _themeController =
+      ThemeController.instance;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _themeController.addListener(
+      _onThemeChanged,
+    );
+  }
+
+  @override
+  void dispose() {
+    _themeController.removeListener(
+      _onThemeChanged,
+    );
+    super.dispose();
+  }
+
+  void _onThemeChanged() {
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'WeLibre',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.dark(),
-      home: const AuthGate(),
+
+      theme:
+          AppTheme.light(),
+
+      darkTheme:
+          AppTheme.dark(),
+
+      themeMode:
+          _themeController.themeMode,
+
+      home:
+          const AuthGate(),
     );
   }
 }
@@ -27,11 +73,16 @@ class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+  ) {
     return AnimatedBuilder(
-      animation: AuthSession.instance,
+      animation:
+          AuthSession.instance,
       builder: (context, _) {
-        if (AuthSession.instance.isAuthenticated) {
+        if (AuthSession
+            .instance
+            .isAuthenticated) {
           return const HomeScreen();
         }
 
