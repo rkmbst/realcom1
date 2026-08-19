f418a9a7f4b57e7fe3d9d2d07b1c7befd61ea948
rkmbst <khaouni0rabah13@gmail.com>
2026-08-19 16:04:36 +0100

    Add methods to evaluate question pack types
    Added methods to check if the question pack contains quiz questions or is an opinion pack.

--- lib/models/question_pack.dart
+++ lib/models/question_pack.dart
@@ -12,4 +12,21 @@ class QuestionPack {
     required this.title,
     required this.questions,
   });
+
+  /// Whether this pack contains at least one quiz question.
+  bool get containsQuiz {
+    return questions.any(
+      (question) =>
+          question.type == QuestionType.quiz,
+    );
+  }
+
+  /// Whether every question in this pack
+  /// is a poll/opinion/discussion question.
+  bool get isOpinionPack {
+    return questions.every(
+      (question) =>
+          question.type != QuestionType.quiz,
+    );
+  }
 }
