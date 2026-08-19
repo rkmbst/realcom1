d7de9f1b4ff13ab4e84272f9e652e417f3a6b670
rkmbst <khaouni0rabah13@gmail.com>
2026-08-19 01:48:59 +0100

    Update app_physics.dart

--- lib/core/theme/app_physics.dart
+++ lib/core/theme/app_physics.dart
@@ -33,3 +33,77 @@ class AppPhysics {
     parent: AlwaysScrollableScrollPhysics(),
   );
 }
+
+class VerticalFeedPhysics extends PageScrollPhysics {
+  const VerticalFeedPhysics({
+    super.parent,
+  });
+
+  @override
+  VerticalFeedPhysics applyTo(
+    ScrollPhysics? ancestor,
+  ) {
+    return VerticalFeedPhysics(
+      parent: buildParent(ancestor),
+    );
+  }
+
+  @override
+  Simulation? createBallisticSimulation(
+    ScrollMetrics position,
+    double velocity,
+  ) {
+    if (position.outOfRange) {
+      return super.createBallisticSimulation(
+        position,
+        velocity,
+      );
+    }
+
+    final viewport =
+        position.viewportDimension;
+
+    if (viewport <= 0) {
+      return null;
+    }
+
+    final currentPage =
+        position.pixels / viewport;
+
+    int targetPage;
+
+    if (velocity > 300) {
+      targetPage = currentPage.ceil();
+    } else if (velocity < -300) {
+      targetPage = currentPage.floor();
+    } else {
+      targetPage = currentPage.round();
+    }
+
+    final maxPage =
+        (position.maxScrollExtent / viewport)
+            .round();
+
+    targetPage =
+        targetPage.clamp(0, maxPage);
+
+    final targetPixels =
+        targetPage * viewport;
+
+    if ((targetPixels - position.pixels)
+            .abs() <
+        toleranceFor(position).distance &&
+        velocity.abs() <
+            toleranceFor(position).velocity) {
+      return null;
+    }
+
+    return ScrollSpringSimulation(
+      AppPhysics.videoSpring,
+      position.pixels,
+      targetPixels,
+      velocity,
+      tolerance: toleranceFor(position),
+    );
+  }
+}
